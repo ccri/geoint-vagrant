ebfbe74e0b45c32b88bc21cdb61e1d6a25621183
Tom Kunicki <tkunicki@ccri.com>
2016-03-02 04:56:46 -0600

    added puppet services and skeleton configs

--- data/vagrant-head.sh
+++ data/vagrant-head.sh
@@ -2,3 +2,15 @@
 
 sh /vagrant/data/vagrant-ssh.sh
 
+chkconfig iptables on
+service iptables stop
+
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+
+yum install puppet-server -y
+
+cp -vRT /vagrant/data/head /etc/puppet
+chown -vR puppet:puppet /etc/puppet
+
+chkconfig puppetmaster on
+service puppetmaster start
--- data/vagrant-worker.sh
+++ data/vagrant-worker.sh
@@ -2,3 +2,15 @@
 
 sh /vagrant/data/vagrant-ssh.sh
 
+chkconfig iptables on
+service iptables stop
+
+rpm -ivh https://yum.puppetlabs.com/puppetlabs-release-el-6.noarch.rpm
+
+yum install puppet -y
+
+cp -vRT /vagrant/data/worker /etc/puppet
+chown -vR puppet:puppet /etc/puppet
+
+chkconfig puppet off
+service puppet stop
